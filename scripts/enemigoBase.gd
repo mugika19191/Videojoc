9dd60f7afba605f0b58c6d80d5febc309e696dab
acasales <acasales003@ikasle.ehu.eus>
2023-09-25 12:15:07 +0200

    Combate por turnos
    Boltereta

---
+++ scripts/enemigoBase.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+@export var nombre = "Enemy"
+@export var sprite = Texture.new()
+@export var vida = 20
+@export var daño = 5
---
+++ scripts/state.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var vida_actual = 50
+var vida_max = 50
+var daño = 10
+var defensa = 2
