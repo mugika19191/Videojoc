4b115e02da9d6688a84579794367e74299c8d66e
Patricia Ortega Garcia <patricia.ortega.garcia.02@gmail.com>
2023-10-03 20:46:52 +0200

    He añadido 3 diccionarios donde se listan todos los amuletos, armaduras y armas, junto con sus atributos y efectos, y he implementado el código que analiza que objetos tiene el personaje en el inventario para actualizar sus estadísticas.

---
+++ scripts/objetos.gd
@@ -0,0 +1,347 @@
+extends Resource
+
+# Diccionario de Armaduras
+var armaduras = {
+  "301": {
+	"Nombre": "Placas de Hierro",
+	"Descripcion": "Aumenta la resistencia y la vida máxima del jugador.",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Defensa": 2,
+	  "Vida": 10,
+	  "Resistencia": "Fisica"
+	}
+  },
+  "302": {
+	"Nombre": "Túnica del Viento",
+	"Descripcion": "Aumenta la velocidad de movimiento y la destreza.",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Defensa": 2,
+	  "Destreza": 1,
+	  "Velocidad": 10
+	}
+  },
+  "303": {
+	"Nombre": "Manto de las Sombras",
+	"Descripcion": "Proporciona invisibilidad temporal cuando el jugador tiene menos del 10% de su vida.",
+	"Rareza": "Legendario",
+	"Efecto": {
+	  "Defensa": 3,
+	  "invisibilidad": true
+	}
+  },
+  "304": {
+	"Nombre": "Piel de Dragón",
+	"Descripcion": "Ofrece una resistencia significativa al fuego y el daño de quemaduras.",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Defensa": 2,
+	  "Vida": 12,
+	  "Resistencia": "Fuego"
+	}
+  },
+  "305": {
+	"Nombre": "Armadura de Cristal",
+	"Descripcion": "Aumenta la inteligencia del jugador y su capacidad de lanzar hechizos.",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Defensa": 2,
+	  "Inteligencia": 1,
+	  "Maná": 10
+	}
+  },
+  "306": {
+	"Nombre": "Cota de Mallas Ligera",
+	"Descripcion": "Aumenta la destreza.",
+	"Rareza": "Raro",
+	"Efecto": {
+	  "Defensa": 1,
+	  "Destreza": 1
+	}
+  },
+  "307": {
+	"Nombre": "Placas del Gigante",
+	"Descripcion": "Mejora la fuerza del jugador y reduce el daño físico recibido.",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Defensa": 2,
+	  "Fuerza": 1,
+	  "Resistencia": "Fisica"
+	}
+  },
+  "308": {
+	"Nombre": "Armadura de Escarcha",
+	"Descripcion": "Proporciona resistencia al daño de hielo y aumenta el poder de los hechizos de hielo.",
+	"Rareza": "Legendario",
+	"Efecto": {
+	  "Defensa": 3,
+	  "Resistencia": "Hielo",
+	  "Daño de Hielo": 7
+	}
+  },
+  "309": {
+	"Nombre": "Vestiduras de la Luz",
+	"Descripcion": "Incrementa la velocidad de regeneración de vida.",
+	"Rareza": "Legendario",
+	"Efecto": {
+	  "Defensa": 3,
+	  "Regeneracion": 10
+	}
+  },
+  "310": {
+	"Nombre": "Capa Élfica",
+	"Descripcion": "Aumenta la destreza y la probabilidad de esquivar ataques.",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Defensa": 2,
+	  "Destreza": 1,
+	  "Esquive": 10
+	}
+  }
+}
+
+
+# Diccionario de Amuletos
+var amuletos = {
+  "201": {
+	"Nombre": "Amuleto de la Fuerza",
+	"Descripcion": "Aumenta la fuerza del personaje.",
+	"Rareza": "Raro",
+	"Efecto": {
+	  "Fuerza": 1
+	}
+  },
+  "202": {
+	"Nombre": "Amuleto de la Destreza",
+	"Descripcion": "Aumenta la destreza del personaje.",
+	"Rareza": "Raro",
+	"Efecto": {
+	  "Destreza": 1
+	}
+  },
+  "203": {
+	"Nombre": "Amuleto del Poder",
+	"Descripcion": "Aumenta la poder del personaje.",
+	"Rareza": "Raro",
+	"Efecto": {
+	  "Poder": 1
+	}
+  },
+  "204": {
+	"Nombre": "Amuleto de la Inteligencia",
+	"Descripcion": "Aumenta la inteligencia del personaje.",
+	"Rareza": "Raro",
+	"Efecto": {
+	  "Inteligencia": 1
+	}
+  },
+  "205": {
+	"Nombre": "Amuleto de la Certeza",
+	"Descripcion": "Aumenta la probabilidad de hacer un ataque crítico.",
+	"Rareza": "Epico",
+	"Efecto": {
+	  "Prob Critico": 5
+	}
+  },
+  "206": {
+	"Nombre": "Amuleto del Guerrero",
+	"Descripcion": "Aumenta el daño infligido por armas cuerpo a cuerpo.",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Daño de Armas Melé": 2
+	}
+  },
+  "207": {
+	"Nombre": "Amuleto del Arquero",
+	"Descripcion": "Aumenta el daño infligido por armas a distancia.",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Daño de Armas Distancia": 2
+	}
+  },
+  "208": {
+	"Nombre": "Amuleto del Mago",
+	"Descripcion": "Aumenta el daño infligido de los hechizos lanzados.",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Daño de Hechizos": 2
+	}
+  },
+  "209": {
+	"Nombre": "Amuleto de la Vida",
+	"Descripcion": "Aumenta la vida máxima del jugador.",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Vida": 8
+	}
+  },
+  "210": {
+	"Nombre": "Amuleto de la Resistencia",
+	"Descripcion": "Proporciona resistencia a efectos negativos",
+	"Rareza": "Legendario",
+	"Efecto": {
+	  "Resistencia Efectos": true
+	}
+  },
+  "211": {
+	"Nombre": "Amuleto de la Velocidad",
+	"Descripcion": "Incrementa la velocidad de movimiento.",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Velocidad": 10
+	}
+  },
+  "212": {
+	"Nombre": "Amuleto de la Suerte",
+	"Descripcion": "Mejora la probabilidad de encontrar objetos raros y épicos.",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Suerte": true
+	}
+  },
+  "213": {
+	"Nombre": "Amuleto de Maná",
+	"Descripcion": "Aumenta la capacidad de lanzar hechizos.",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Maná": 10
+	}
+  },
+}
+
+
+# Diccionario de Armas
+var armas = {
+  "101": {
+	"Nombre": "Espada de Fuego",
+	"Descripcion": "Inflige daño de fuego adicional.",
+	"Tipo": "Melé",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Daño Adicional": 3,
+	  "Tipo": "Fuego"
+	}
+  },
+  "102": {
+	"Nombre": "Daga de las Sombras",
+	"Descripcion": "Aumenta la velocidad del jugador y la probabilidad de golpe crítico.",
+	"Tipo": "Lanzable",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Velocidad": 10,
+	  "Prob Critico": 10
+	}
+  },
+  "103": {
+	"Nombre": "Arco Élfico",
+	"Descripcion": "Permite ataques a distancia y aumenta la destreza.",
+	"Tipo": "Distancia",
+	"Rareza": "Raro",
+	"Efecto": {
+	  "Destreza": 1
+	}
+  },
+  "104": {
+	"Nombre": "Hacha de Guerra",
+	"Descripcion": "Ofrece un alto daño físico.",
+	"Tipo": "Melé",
+	"Rareza": "Raro",
+	"Efecto": {
+	  "Daño Adicional": 4,
+	}
+  },
+  "105": {
+	"Nombre": "Báculo Arcano",
+	"Descripcion": "Aumenta el poder de los hechizos y la inteligencia.",
+	"Tipo": "Magico",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Daño de Hechizos": 3,
+	  "Inteligencia": 1
+	}
+  },
+  "106": {
+	"Nombre": "Martillo de Guerra",
+	"Descripcion": "Golpea con fuerza y tiene un efecto de aturdimiento.",
+	"Tipo": "Melé",
+	"Rareza": "Legendario",
+	"Efecto": {
+	  "Daño Adicional": 4,
+	  "Aturdir": true
+	}
+  },
+  "107": {
+	"Nombre": "Lanza del Viento",
+	"Descripcion": "Aumenta la destreza y hace daño adicional.",
+	"Tipo": "Lanzable",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Destreza": 1,
+	  "Daño Adicional": 3
+	}
+  },
+  "108": {
+	"Nombre": "Ballesta Mágica",
+	"Descripcion": "Permite disparos potentes a distancia.",
+	"Tipo": "Distancia",
+	"Rareza": "Raro",
+	"Efecto": {
+	  "Daño Adicional": 4
+	}
+	
+  },
+  "109": {
+	"Nombre": "Espada de Hielo",
+	"Descripcion": "Inflige daño de hielo adicional.",
+	"Tipo": "Melé",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Daño Adicional": 3,
+	  "Tipo": "Hielo"
+	}
+  },
+  "110": {
+	"Nombre": "Escudo de Deflexión",
+	"Descripcion": "Mejora la probabilidad de bloquear los ataques enemigos, aumentando la estadística de Destreza. No puedes atacar con él.",
+	"Tipo": "Escudo",
+	"Rareza": "Épico",
+	"Efecto": {
+	  "Destreza": 1,
+	  "Ataque": false,
+	  "Prob Bloqueo": 10
+	}
+  },
+  "111": {
+	"Nombre": "Garra de Dragón",
+	"Descripcion": "Aumenta la estadística de Fuerza y permite al personaje desgarrar la armadura de los enemigos.",
+	"Tipo": "Melé",
+	"Rareza": "Legendario",
+	"Efecto": {
+	  "Fuerza": 1,
+	  "Desgarrar": true
+	}
+  },
+  "112": {
+	"Nombre": "Hacha Gigante",
+	"Descripcion": "Aumenta la estadística de Fuerza y tiene una probabilidad de realizar un ataque crítico que inflige un gran daño.",
+	"Tipo": "Melé",
+	"Rareza": "Legendario",
+	"Efecto": {
+	  "Fuerza": 1,
+	  "Prob Critico": 5,
+	  "Daño Crítico": 10
+	}
+  }
+}
+
+
+# Agregar funciones para acceder a los diccionarios
+func obtener_diccionario_armaduras():
+	return armaduras
+
+func obtener_diccionario_amuletos():
+	return amuletos
+
+func obtener_diccionario_armas():
+	return armas
--- scripts/state.gd
+++ scripts/state.gd
@@ -1,6 +1,147 @@
 extends Node
 
+
+var objetos = preload("objetos.gd").new()
 var vida_actual = 50
 var vida_max = 50
+var mana_actual = 30
+var mana_max = 30
 var daño = 10
+var des = 0
+var fue = 0
+var itg = 0
+var pod = 0
+var velocidad = 10
 var defensa = 2
+var resistencias = []
+var pasivas = []
+var prob_bloqueo = 0
+
+var inventario = {
+	"armadura": 304, #Ejemplo: la armadura 304 debería aumentar la vida_act a 62, y añadir la resistencia [Fuego]
+	"amuleto": 203, #Ejemplo: la armadura 203 debería aumentar el poder a 1 
+	"armas": [101, 102]  # Dos ranuras para armas #Ejemplo: Daño 13 y Velocidad 20 
+}
+
+func _ready():
+	print("La vida maxima al principio es: ")
+	print(vida_max)
+	calcular_estadisticas()
+	print("Y despues de mirar los objetos la vida maxima es: ")
+	print(vida_max)
+	print(resistencias)
+	print("El poder al final es: ")
+	print(pod)
+	print("El daño final es: ")
+	print(daño)
+	print("La velocidad final es: ")
+	print(velocidad)
+
+func agregar_objeto_a_inventario(tipo_objeto, objeto_id):
+	# Verifica si la ranura del inventario está disponible para el tipo de objeto
+	if inventario.has(tipo_objeto) and inventario[tipo_objeto] == null:
+		inventario[tipo_objeto] = objeto_id
+		
+	# Después de agregar el objeto al inventario, recalculate las estadísticas del personaje
+	calcular_estadisticas()
+
+
+func calcular_estadisticas():
+	#Reestablecemos las estadsticas a los valores base
+	vida_max = 50
+	mana_max = 30
+	daño = 10
+	des = 0
+	fue = 0
+	itg = 0
+	pod = 0
+	velocidad = 10
+	defensa = 2
+	resistencias = []
+	pasivas = []
+	prob_bloqueo = 0
+	
+	#Miramos los objetos del inventario para recalcular las estadisticas
+	if inventario["armadura"]:
+		var armadura_id = str(inventario["armadura"])
+		print("La armadura equipada tiene id: ")
+		print(armadura_id)
+		
+		if objetos.armaduras.has(armadura_id):
+			var armadura_equipada = objetos.armaduras[armadura_id]
+			print("La armadura equipada se llama:")
+			print(armadura_equipada["Nombre"])
+			# Incrementar la defensa basada en el efecto de la armadura si existe
+			if "Efecto" in armadura_equipada:
+				if "Resistencia" in armadura_equipada["Efecto"]:
+					print("La armadura tiene las siguiente resistencias: ")
+					print(armadura_equipada["Efecto"]["Resistencia"])
+					resistencias.append(armadura_equipada["Efecto"]["Resistencia"])
+				
+				if "Invisivilidad" in armadura_equipada["Efecto"]:
+					pasivas.append("Invisibilidad")
+					
+				defensa += armadura_equipada["Efecto"].get("Defensa", 0)
+				vida_max += armadura_equipada["Efecto"].get("Vida", 0)
+				vida_actual += armadura_equipada["Efecto"].get("Vida", 0)
+				velocidad += armadura_equipada["Efecto"].get("Velocidad", 0)
+				mana_max += armadura_equipada["Efecto"].get("Maná", 0)
+				mana_max += armadura_equipada["Efecto"].get("Maná", 0)
+				itg += armadura_equipada["Efecto"].get("Inteligencia", 0)
+				des += armadura_equipada["Efecto"].get("Destreza", 0)
+				fue += armadura_equipada["Efecto"].get("Fuerza", 0)
+				pod += armadura_equipada["Efecto"].get("Poder", 0)
+				
+				#FALTA ARMADURA 308, 309, 310
+
+
+	if inventario["amuleto"]:
+		var amuleto_id = str(inventario["amuleto"])
+		
+		if objetos.amuletos.has(amuleto_id):
+			var amuleto_equipado = objetos.amuletos[amuleto_id]
+			
+			if "Efecto" in amuleto_equipado:
+				itg += amuleto_equipado["Efecto"].get("Inteligencia", 0)
+				des += amuleto_equipado["Efecto"].get("Destreza", 0)
+				fue += amuleto_equipado["Efecto"].get("Fuerza", 0)
+				pod += amuleto_equipado["Efecto"].get("Poder", 0)
+				vida_max += amuleto_equipado["Efecto"].get("Vida", 0)
+				vida_actual += amuleto_equipado["Efecto"].get("Vida", 0)
+				mana_max += amuleto_equipado["Efecto"].get("Maná", 0)
+				mana_max += amuleto_equipado["Efecto"].get("Maná", 0)
+				velocidad += amuleto_equipado["Efecto"].get("Velocidad", 0)
+				
+				#FALTA AMULETO 205, 206, 207, 208, 210, 212
+				
+				
+	for i in range(2):
+		if inventario["armas"][i]:
+			var arma_id = str(inventario["armas"][i])
+			
+			if objetos.armas.has(arma_id):
+				var arma_equipada = objetos.armas[arma_id]
+				
+				if "Efecto" in arma_equipada:
+					itg += arma_equipada["Efecto"].get("Inteligencia", 0)
+					des += arma_equipada["Efecto"].get("Destreza", 0)
+					fue += arma_equipada["Efecto"].get("Fuerza", 0)
+					pod += arma_equipada["Efecto"].get("Poder", 0)
+					daño += arma_equipada["Efecto"].get("Daño Adicional", 0)
+					velocidad += arma_equipada["Efecto"].get("Velocidad", 0)
+					prob_bloqueo += arma_equipada["Efecto"].get("Prob Bloqueo", 0)
+					
+					#FAlTAN ARMAS (faltan bastantes "Efectos")
+			
+func obtener_des():
+	return des
+func obtener_itg():
+	return itg
+func obtener_fue():
+	return fue
+func obtener_pod():
+	return pod
+func obtener_vidaAct():
+	return vida_actual
+func obtener_vidaMax():
+	return vida_max
