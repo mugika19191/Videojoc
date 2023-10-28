a8c35edb612da500d38144e154a8221af28fd6d1
Mugika <mugika1919@gmail.com>
2023-10-29 00:02:30 +0200

    Animacion al empezar y cambios del nivel
    Se han cambiado los bloques del menu de inicio por modelos 3d ademas de implemetar una animacion para empezar.

--- scripts/inicio.gd
+++ scripts/inicio.gd
@@ -9,4 +9,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene_to_file("res://mapas/Prototipo.tscn")
+		AnimationLevel.change_scene_to_file("res://mapas/Prototipo.tscn")
