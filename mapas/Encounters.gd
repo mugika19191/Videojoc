a67f648de457660e36a3586218d97eff25911553
Mugika <mugika1919@gmail.com>
2023-11-14 00:49:00 +0100

    Combates aleatorios añadidos

---
+++ mapas/Encounters.gd
@@ -0,0 +1,27 @@
+extends WorldEnvironment
+
+@export var combate : PackedScene
+var rng = RandomNumberGenerator.new()
+var com=0
+var rem=false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Replace with function body.
+	pass
+	
+		
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if $"../Player".velocity.x !=0 || $"../Player".velocity.z !=0:
+		rng.randomize()
+		com=rng.randi_range(0,200)
+		if (com<2):
+			get_tree().paused=!get_tree().paused
+			var comateI := combate.instantiate()
+			add_child(comateI)	
+
+
+func _on_child_entered_tree(node):
+	if get_child_count()>1:
+		remove_child(get_child(get_child_count()-2))
