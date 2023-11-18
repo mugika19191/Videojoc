a189b49d65be6c8fa7c10a9146923cd3adce564b
Zodiak811 <galderlafuente@gmail.com>
2023-11-18 13:20:22 +0100

    Enemigo final implementado con su pista de audio correspondiente
    La sala final está completamente funcional. Si algo falla, avisar.

---
+++ scripts/NPC1.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+@export var combate : PackedScene
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if $Sprite3D2.visible && Input.is_action_just_pressed("ui_accept") && !get_tree().paused:
+		AnimationLevel.change_scene_to_file("res://nodos/combate.tscn")
+
+func _on_area_3d_area_entered(area):
+	# Replace with function body.
+	$Sprite3D2.visible=true
+
+func _on_area_3d_area_exited(area):
+	$Sprite3D2.visible=false
